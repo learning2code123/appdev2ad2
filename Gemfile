81c7ea769f87fda8bd93c9439316581fc7a969ad
learning2code123 <lmattos@chicagobooth.edu>
2023-01-10 14:37:05 +0000

    changes 1

--- Gemfile
+++ Gemfile
@@ -3,6 +3,10 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '3.0.3'
 
+# add gem
+
+gem "draft_generators", github: "firstdraft/draft_generators"
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.3', '>= 6.1.3.1'
 # Use postgresql as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,32 @@
 Rails.application.routes.draw do
+  
+  #get "/", controller: "movies", action: "new"
+
+  #get "/" => "movies#index"
+
+  root "movies#index"
+
+  # Routes for the Movie resource:
+
+  # CREATE
+  post "/movies" => "movies#create", as: :movies #movies_url and movies_path
+  get "/movies/new" => "movies#new", as: :new_movie #new_movie_url and new_movie_path 
+          
+  # READ (usually get indicates reading)
+  get "/movies" => "movies#index" 
+  
+  get "/movies/:id" => "movies#show", as: :movie #movie_path() expects an argument to populate the :id segment
+  
+  # UPDATE
+  
+  patch "/movies/:id" => "movies#update" 
+  get "/movies/:id/edit" => "movies#edit", as: :edit_movie #edit_movie_path() expects an argument to populate the :id segment
+  
+  # DELETE
+  delete "/movies/:id" => "movies#destroy" 
+
+  #------------------------------
+
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

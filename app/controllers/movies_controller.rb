29824f422ecbac54ccf8d8ea9c83f68ceea05ae5
learning2code123 <lmattos@chicagobooth.edu>
2023-01-10 19:31:25 +0000

    changes 7

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,11 +7,11 @@ class MoviesController < ApplicationController
   def index
     matching_movies = Movie.all
 
-    @list_of_movies = matching_movies.order(created_at: :desc)
+    @movies = matching_movies.order(created_at: :desc)
 
     respond_to do |format|
       format.json do
-        render json: @list_of_movies
+        render json: @movies
       end
       format.html
     end
@@ -23,11 +23,11 @@ class MoviesController < ApplicationController
 
   def create
     @movie = Movie.new
-    @movie.title = params.fetch("query_title")
-    @movie.description = params.fetch("query_description")
-    @movie.released = params.fetch("query_released", false)
+    @movie.title = params.fetch("title")
+    @movie.description = params.fetch("description")
+    #@movie.released = params.fetch("query_released", false)
 
-    if movie.valid?
+    if @movie.valid?
       @movie.save
       redirect_to movies_url, notice: "Movie created successfully." 
     else
@@ -48,9 +48,9 @@ class MoviesController < ApplicationController
   def update
     @movie = Movie.find(params.fetch(:id))
 
-    movie.title = params.fetch("query_title")
-    movie.description = params.fetch("query_description")
-    movie.released = params.fetch("query_released", false)
+    @movie.title = params.fetch("query_title")
+    @movie.description = params.fetch("query_description")
+    @movie.released = params.fetch("query_released", false)
 
     if movie.valid?
       movie.save

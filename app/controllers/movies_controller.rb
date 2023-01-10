936519b6f14b82d73bcba106952d38d8e3728db0
learning2code123 <lmattos@chicagobooth.edu>
2023-01-10 21:52:11 +0000

    changes 8

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,9 +5,9 @@ class MoviesController < ApplicationController
   end
   
   def index
-    matching_movies = Movie.all
+    #matching_movies = Movie.all
 
-    @movies = matching_movies.order(created_at: :desc)
+    @movies = Movie.order(created_at: :desc)
 
     respond_to do |format|
       format.json do
@@ -22,9 +22,12 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.new
-    @movie.title = params.fetch(:movie).fetch(:title)
-    @movie.description = params.fetch(:movie).fetch(:description)
+
+    movie_attributes = params.require(:movie).permit(:title, :description)
+    @movie = Movie.new(movie_attributes)
+    
+    #@movie.title = params.fetch(:movie).fetch(:title)
+    #@movie.description = params.fetch(:movie).fetch(:description)
     #@movie.released = params.fetch("query_released", false)
 
     if @movie.valid?
@@ -48,22 +51,22 @@ class MoviesController < ApplicationController
   def update
     @movie = Movie.find(params.fetch(:id))
 
-    @movie.title = params.fetch("query_title")
-    @movie.description = params.fetch("query_description")
-    @movie.released = params.fetch("query_released", false)
+    @movie.title = params.fetch(:title)
+    @movie.description = params.fetch(:description)
+    #@movie.released = params.require(:movie).permit(:title)
 
-    if movie.valid?
-      movie.save
-      redirect_to movie_url(the_movie), notice: "Movie updated successfully."
+    if @movie.valid?
+      @movie.save
+      redirect_to movie_url(@movie), notice: "Movie updated successfully."
     else
-      redirect_to movie_url(the_movie), alert: the_movie.errors.full_messages.to_sentence 
+      redirect_to movie_url(@movie), alert: @movie.errors.full_messages.to_sentence 
     end
   end
 
   def destroy
     @movie = Movie.find(params.fetch(:id))
 
-    movie.destroy
+    @movie.destroy
 
     redirect_to movies_url, notice: "Movie deleted successfully."
   end
